f89ab67b46342a76c05ba2705196ab9890192596
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2024-04-13 14:35:11 +0200

    add constants file

---
+++ Dgtera Task/Consts.swift
@@ -0,0 +1,14 @@
+//
+//  Consts.swift
+//  Dgtera Task
+//
+//  Created by Walid Ahmed on 13/04/2024.
+//
+
+import Foundation
+class Consts{
+    static var BASE_URL = "https://comu_test_tomtom.dgtera.com/"
+    static var WEB_KEY = BASE_URL + "web/"
+    static var LOGIN = WEB_KEY + "session/authenticate"
+    static var PRODUCTS = WEB_KEY + "dataset/call_kw"
+}
